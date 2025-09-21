12a056f58874035b3f447050938c979d42e624d6
AdamFeiyangLiu <liufeiyang53004982@163.com>
2025-09-21 15:05:37 -0600

    2x faster with mtk

--- programming_examples/basic/matrix_multiplication_2/matrix_vector/matrix_vector.py
+++ programming_examples/basic/matrix_multiplication_2/matrix_vector/matrix_vector.py
@@ -19,6 +19,8 @@ def my_matmul(dev, M, K):
  
     m = 32
     k = 32
+    mtk = 512
+    mtk_div_k = mtk // k 
 
     n_cols = 4
     n_rows = 4 
@@ -55,7 +57,7 @@ def my_matmul(dev, M, K):
 
         @device(dev_ty)
         def device_body():
-            inA_ty = np.ndarray[(m * k * 4,), dtype_in]
+            inA_ty = np.ndarray[(m* mtk_div_k *4,k), dtype_in]
             inB_ty = np.ndarray[(k,), dtype_in]
             MemC_ty = np.ndarray[(m * 4,), dtype_out]
             outC_ty = np.ndarray[(m,), dtype_out]
@@ -70,17 +72,9 @@ def my_matmul(dev, M, K):
             )
 
             # Tile declarations
-            ShimTile0 = tile(0, 0)
-            ShimTile1 = tile(1, 0)
-            ShimTile2 = tile(2, 0)
-            ShimTile3 = tile(3, 0)
-            ShimTiles = [ShimTile0, ShimTile1, ShimTile2, ShimTile3]
-            MemTile0 = tile(0, 1)
-            MemTile1 = tile(1, 1)
-            MemTile2 = tile(2, 1)
-            MemTile3 = tile(3, 1)
-            MemTiles = [MemTile0, MemTile1, MemTile2, MemTile3]
-            memA_fifos = []
+            ShimTiles = [tile(i,0) for i in range(n_cols)]
+            MemTiles = [tile(i,1) for i in range(n_cols)]
+            memA_fifos = [None] * n_cols
             inA_fifos = [[None] * n_cols for _ in range(n_rows)]
             outC_fifos = [[None] * n_cols for _ in range(n_rows)]
             inB_fifos = [None] * n_cols
@@ -95,29 +89,47 @@ def my_matmul(dev, M, K):
             ]
             # AIE-array data movement with object fifos
             # Input A
+            for i in range(n_cols):
+                memA_fifos[i] = object_fifo(
+                    f"mem_A{i}",
+                    (
+                        ShimTiles[i]
+                    ),  
+                    MemTiles[i],
+                    2,
+                    inA_ty,
+                    None,
+                    # S2MM in MemTile to convert m * mtk block from row-maj
+                    # to tiled m*k blocks
+                    # [
+                    #     [
+                    #         (m, k),
+                    #         (mtk // k, m * k),
+                    #         (k, 1),
+                    #     ]
+                       
+                    # ],
+            )
             for col in range(n_cols):
-                memA_fifos.append(
-                    object_fifo(f"memA{col}", ShimTiles[col], MemTiles[col], 2, inA_ty)
-                )
                 for row in range(n_rows):
                     inA_fifos[row][col]= object_fifo(
                             f"inA{row}{col}",
                             MemTiles[col],
                             cores[row][col],
-                            4,
+                            2,
                             A_ty,
-                            (
-                                [
+                            #MM2S
+                            # None,
+                            
+                                [   
                                     (k  // 2, 2),
                                     (m, k),
                                     (2, 1),
                                 ]
-                                if vectorized
-                                else []
-                            ),  # transpose at 4-byte (2xbf16) granularity
+                            
                         )
                 
-                object_fifo_link(memA_fifos[col], [inA_fifos[row][col] for row in range(n_rows)],[],[m*k*j for j in range(n_rows)])#
+                object_fifo_link(memA_fifos[col], [inA_fifos[row][col] for row in range(n_rows)],[],[m*mtk*j for j in range(n_rows)])#
 
                 # Output C
                 memC_fifos.append(
@@ -206,8 +218,8 @@ def my_matmul(dev, M, K):
                             A_base_offset =  j * K * M // num_iter + pingpong * K * M // num_iter // 2
                             A_col_offset = col * K * M // num_iter // 2 // n_cols
                             A_offset = A_base_offset + A_col_offset
-                            A_sizes = [1, K_div_k, n_rows * m, k]
-                            A_strides = [0, k, K, 1]
+                            A_sizes = [1, K//mtk, n_rows * m, mtk]
+                            A_strides = [0, mtk, K, 1]
                             npu_dma_memcpy_nd(
                                     metadata=memA_fifos[col],
                                     bd_id=bd_id_base + 1,
